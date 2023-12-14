cd5cfce8505e6ba2f9ea97218117ad514612e906
elisabichiappini <elisabichiappini@gmail.com>
2023-12-14 20:12:53 +0100

    feat: milestone 0

--- src/store.js
+++ src/store.js
@@ -2,10 +2,16 @@
 import { reactive } from "vue";
 //esportazione dell'oggetto creato per la comunicazione globale 
 export const store = reactive ({
+    //api ricerca movies
     apiUrlMovie: 'https://api.themoviedb.org/3/search/movie?api_key=607624ff950bb1869c240ced7f245851&query=',
+    //api ricerca tv
     apiUrlSeriesTv: 'https://api.themoviedb.org/3/search/tv?api_key=607624ff950bb1869c240ced7f245851&query=',
+    //dati inseriti nella search
     searchKey: '',
+    //array di movies restituiti dopo la ricerca
     movies:[],
+    //array di seriesTv restituiti dopo la ricerca
     series:[],
+    //api da implementare per la restituzione delle immagini di movies e seriesTv
     apiPoster:'image.tmdb.org/t/p/',
 });
